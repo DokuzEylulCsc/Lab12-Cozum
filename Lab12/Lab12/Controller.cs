5f0a609ccd2b9f327b9009d1ae97be7362f6423f
Alican <alican.dogan@deu.edu.tr>
2019-05-24 08:46:16 +0300

    Cozum eklendi

---
+++ Lab12/Lab12/Controller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab12
+{
+    class Controller : IController
+    {
+        private IModel model;
+        private IView view;
+
+
+
+        public void getRequestFromModel()
+        {
+            model.getRequest();
+        }
+
+        public void setView(IView v)
+        {
+            view = v;
+        }
+
+        public void setModel(IModel m)
+        {
+            model = m;
+        }
+
+        public void updateView(string input)
+        {
+            view.update(input);
+        }
+    }
+}
---
+++ Lab12/Lab12/View.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab12
+{
+    class View : IView
+    {
+        public void update(string input)
+        {
+            Console.WriteLine(input);
+        }
+    }
+}
